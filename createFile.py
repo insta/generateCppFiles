8baa610cf38abb8297862c389d54f90c0ce7fb0a
etnarek <romain-fontaine@outlook.com>
2013-05-23 21:27:51 +0200

    refactor code using pep8

--- createFile.py
+++ createFile.py
@@ -1,43 +1,46 @@
 def createFile(fileNames):
-	for fileName in fileNames:
-		createCpp(fileName)
-		createHpp(fileName)
+    for fileName in fileNames:
+        createCpp(fileName)
+        createHpp(fileName)
+
 
 def createCpp(fileName):
-	data=["using namespace std;"]
-	writeInFile(fileName+".cpp",data)
+    data = ["using namespace std;"]
+    writeInFile(fileName + ".cpp", data)
+
 
 def createHpp(fileName):
-	data=["#IFNDEF "+fileName.upper()+"_HPP","#DEFINE "+fileName.upper()+"_HPP","#ENDIF"]
-	writeInFile(filename+".hpp",data)
+    data = ["#IFNDEF "+fileName.upper()+"_HPP", "#DEFINE "+fileName.upper()+"_HPP", "#ENDIF"]
+    writeInFile(fileName+".hpp", data)
 
-def writeInFile(fileName,data):	
-	
-	toWrite=open(fileName,"w")
-	for line in data:
-		toWrite.write(data)
-	toWrite.close()
+
+def writeInFile(fileName, data):
+    toWrite = open(fileName, "w")
+    for line in data:
+        toWrite.write(data)
+    toWrite.close()
+
 
 def createClass(fileNames):
 
-	for fileName in fileNames:
-		createCppClass(fileName)
-		createHppClass(fileName)
+    for fileName in fileNames:
+        createCppClass(fileName)
+        createHppClass(fileName)
+
 
 def createHppClass(fileName):
-	
-	data=["#IFNDEF "+fileName.upper()+"_HPP","#DEFINE "+fileName.upper()+"_HPP","#include <iostream> "]
-	data.append("class "+fileName+":")
-	data.append("{")
-	data.append("	public :")
-	data.append("		"+fileName+"();")
-	data.append("		~"+fileName+"();")
-	data.append("	private :")
-	data.append("};")
-	
-	writeInFile(fileName+".hpp",data)
+    data = ["#IFNDEF "+fileName.upper()+"_HPP", "#DEFINE "+fileName.upper()+"_HPP", "#include <iostream> "]
+    data.append("class "+fileName+":")
+    data.append("{")
+    data.append("   public :")
+    data.append("       "+fileName+"();")
+    data.append("       ~"+fileName+"();")
+    data.append("   private :")
+    data.append("};")
+
+    writeInFile(fileName+".hpp", data)
+
 
 def createCppClass(fileName):
-	
-	data=["using namespace std;",fileName+"::"+fileName+"()","{i\n","}\n",fileName+"::~"+fileName+"()","{\n","}"]
-	writeInFile(fileName+".cpp",data)		
+    data = ["using namespace std;", fileName+"::"+fileName+"()", "{i\n", "}\n", fileName+"::~"+fileName+"()", "{\n", "}"]
+    writeInFile(fileName+".cpp", data)
--- main.py
+++ main.py
@@ -1,75 +1,86 @@
 import sys
+from createFile import *
+
+
+def getArgs():
+    mainName = "main.cpp"
+    classNames = []
+    fileNames = []
+    flag = ""
+    counter = 1
+    data = True
+    for arg in sys.argv[1:]:
+        if "-" in arg:
+            if arg == "-c" or arg == "-C":
+                flag = "c"
+                data = False
+            elif arg == "-f" or arg == "-F":
+                flag = "f"
+                data = False
+            else:
+                warning(3)
+        else:
+            if flag == "c":
+                if arg in classNames or arg in fileNames:
+                    warning(2)
+                else:
+                    classNames.append(arg)
+                    data = True
+            elif flag == "f":
+                if arg in fileNames or arg in classNames:
+                    warning(2)
+                else:
+                    fileNames.append(arg)
+                    data = True
+            else:
+                if counter > 1:
+                    warning(0)
+                else:
+                    mainName = arg
+                    counter += 1
+    if not data:
+        warning(1)
+    return mainName, classNames, fileNames
+
+
 def createMain(name, fileName, className):
-	fileOpen = open(name+".cpp", w)
-	fileOpen.write('#include <iostream>')
-	fileOpen.write('\n')
-	for i in fileName:
-		fileOpen.write('#include "'+ i+ '.hpp"')
-	for i in className:
-		fileOpen.write('#include "'+ i +'i.hpp"')
-	fileOpen.write('\n')
-	fileOpen.write('using namespace std;')
-	fileOpen.write('\n')
-	fileOpen.write('int main(int argc, char *argv[])')
-	fileOpen.write('{')
-	fileOpen.write('    cout << "Hello world!" << endl;')
-	fileOpen.write('    return 0;')
-	fileOpen.write('}')
-	fileOpen.close()
+    fileOpen = open(name+".cpp", 'w')
+    fileOpen.write('#include <iostream>')
+    fileOpen.write('\n')
+    for i in fileName:
+        fileOpen.write('#include "' + i + '.hpp"')
+    for i in className:
+        fileOpen.write('#include "' + i + 'i.hpp"')
+    fileOpen.write('\n')
+    fileOpen.write('using namespace std;')
+    fileOpen.write('\n')
+    fileOpen.write('int main(int argc, char *argv[])')
+    fileOpen.write('{')
+    fileOpen.write('    cout << "Hello world!" << endl;')
+    fileOpen.write('    return 0;')
+    fileOpen.write('}')
+    fileOpen.close()
+
 
 def warning(errNum):
-	if errNum==0:
-		print("Flag missing")
-	elif errNum==1:
-		print("Missing arguments after flag delaration")
-	elif errNum==2:
-		print("Double occurence of file")
-	elif errNum==3:
-		print("Unknown flag occurence")
-	else:
-		print("I'm a teapot")
-	sys.exit(0)
+    if errNum == 0:
+        print("Flag missing")
+    elif errNum == 1:
+        print("Missing arguments after flag delaration")
+    elif errNum == 2:
+        print("Double occurence of file")
+    elif errNum == 3:
+        print("Unknown flag occurence")
+    else:
+        print("I'm a teapot")
+    sys.exit(0)
+
 
 def main():
-	mainName="main.cpp"
-	classNames=[]
-	fileNames=[]
-	flag=""
-	counter=1
-	data=True
-	for arg in sys.argv[1:]:
-		if "-" in arg:
-			if arg=="-c" or arg=="-C":
-				flag="c"
-				data=False
-			elif arg=="-f" or arg=="-F":
-				flag="f"
-				data=False
-			else:
-				warning(3)
-		else:
-			if flag=="c":
-				if arg in classNames or arg in fileNames:
-					warning(2)
-				else:
-					classNames.append(arg)
-					data=True
-			elif flag=="f":
-				if arg in fileNames or arg in classNames:
-					warning(2)
-				else:
-					fileNames.append(arg)
-					data=True
-			else:
-				if counter>1:
-					warning(0)
-				else:
-					mainName=arg
-					counter+=1
-	if not data:
-		warning(1)
-	print(fileNames)
-	print(classNames)
-	print(mainName)
+    fileNames, classNames, mainName = getArgs()
+    print(fileNames)
+    print(classNames)
+    print(mainName)
 
-main()	
+if __name__ == '__main__':
+    main()
